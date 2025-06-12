a32782459b8185883a24cf9caa54b39adbcbb0ff
shaanpatel2258 <shaanpatel2258@gmail.com>
2025-06-12 14:03:18 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/shaan-ballin-portfolio",
   server: {
     host: "::",
     port: 8080,
