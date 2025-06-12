a67fcf21e984d11541a4a08903f3c0307ea59479
shaanpatel2258 <shaanpatel2258@gmail.com>
2025-06-12 14:45:19 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,19 @@
 import { defineConfig } from "vite";More actions
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import fs from "fs";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/shaan-ballin-portfolio/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
-    {
-      name: "create-cname-file",
-      closeBundle: () => {
-        // Ensure the dist directory exists, then write CNAME file
-        if (!fs.existsSync("dist")) {
-          fs.mkdirSync("dist");
-        }
-        fs.writeFileSync("dist/CNAME", "shaanpatel.dev");
-      },
-    },
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
